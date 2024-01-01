2ed3bbe67b89aaa419442e23bd560c7b9af97748
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-01 14:00:00 +0300

    Added display of categories

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DataHandlerService} from "../../services/data-handler.service";
+import {Category} from "../../models/Category";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
 
+  categories: Category[] = [];
+
+  constructor(private dataHandlerService: DataHandlerService) {
+  }
+
+  ngOnInit(): void {
+    this.categories = this.dataHandlerService.getCategories();
+  }
 }
