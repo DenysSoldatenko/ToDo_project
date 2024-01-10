47f719ba41f4162996ace6cb5ef2e3484d147cf5
DenysSoldatenko <soldatenko911@gmail.com>
2024-01-10 10:00:00 +0300

    Added MatTable for tasks

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,30 +1,57 @@
 import {Component} from '@angular/core';
 import {Task} from "../../models/Task";
 import {DataHandlerService} from "../../services/data-handler.service";
-import {DatePipe, NgForOf} from "@angular/common";
+import {DatePipe, NgClass, NgForOf} from "@angular/common";
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from "@angular/material/table";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
     NgForOf,
-    DatePipe
+    DatePipe,
+    MatTable,
+    MatHeaderCell,
+    MatCell,
+    MatColumnDef,
+    MatRow,
+    MatHeaderRow,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
+    NgClass
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
 
-  tasks: Task[] = [];
+  protected tasks: Task[] = [];
+  protected displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  protected dataSource: MatTableDataSource<Task> = new MatTableDataSource();
 
   constructor(private dataHandlerService: DataHandlerService) {
   }
 
   ngOnInit() {
     this.dataHandlerService.tasksSubject.subscribe(tasks => this.tasks = tasks);
+    this.refreshTable();
   }
 
-  toggleTaskCompleted(task: Task) {
-    task.completed = !task.completed;
+  getPriorityColor(task: Task) {
+    return task.priority?.color ?? '#fff';
+  }
+
+  private refreshTable() {
+    this.dataSource.data = this.tasks;
   }
 }
